729a8481db74a73ef757a3c78a54bdca8773979d
tms-gvd <thomas.gaviard@gmail.com>
2024-01-27 16:41:12 +0100

    refactored and slightly improved the plotting functions

--- src/utils.py
+++ src/utils.py
@@ -4,13 +4,16 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from src.policies import PolicyFromQ
 
-def get_one_episode(mdp, q):
-    if q.sum() == 0:
-        raise ValueError("The Q matrix is null.")
+
+def get_one_episode(mdp, q, verbose=False):
+    # if q.sum() == 0:
+    #     raise ValueError("The Q matrix is null.")
     pol_q = PolicyFromQ(mdp, q).get_action
     score, t, path = mdp.one_episode(pol_q)
-    print(f"One episode: score = {score}, time = {t}")
+    if verbose:
+        print(f"One episode: time = {t}")
     return score, t, path
+
 
 def viz_path(path, grid, ax=None):
     if ax is None:
@@ -23,44 +26,48 @@ def viz_path(path, grid, ax=None):
         x2, y2 = path[i+1]//ncols, path[i+1]%ncols
         ax.plot([y1, y2], [x1, x2], c='r', zorder=i)
 
-def plot_maze_q_star(grid, q_star):
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
-    
-    img1 = ax1.imshow(grid, cmap='gray')
-    ax1.set_title("Maze")
-    ax1.set_xticks([])  # remove x-axis ticks
-    ax1.set_yticks([])  # remove y-axis ticks
 
-    img2 = ax2.imshow(np.max(q_star, axis=-1).reshape(grid.shape), cmap='hot_r')
-    ax2.set_title(r"$\max_a \ Q^*(s, a)$")
-    ax2.set_xticks([])  # remove x-axis ticks
-    ax2.set_yticks([])  # remove y-axis ticks
+def plot_maze(grid, ax=None):
+    if ax is None:
+        ax = plt.gca()
+    ax.imshow(grid, cmap='gray')
+    ax.set_title("Maze")
+    ax.set_xticks([])  # remove x-axis ticks
+    ax.set_yticks([])  # remove y-axis ticks
+
+
+def plot_q(q, grid, ax=None):
+    qc = q.copy()
+    if ax is None:
+        ax = plt.gca()
+        
+    mask = qc.sum(axis=-1) > 0
+    qc[~mask] = np.nan
+    qc[mask] = np.log(qc[mask])
+    img = ax.imshow(np.max(qc, axis=-1).reshape(grid.shape), cmap='Reds')
+    ax.imshow(np.where(grid==1., np.nan, 1.), cmap='gray')
+    ax.set_xticks([])  # remove x-axis ticks
+    ax.set_yticks([])  # remove y-axis ticks
 
     # Create a new axes for the colorbar that is the same height as ax2
-    divider = make_axes_locatable(ax2)
+    divider = make_axes_locatable(ax)
     cax = divider.append_axes("right", size="5%", pad=0.15)
-    fig.colorbar(img2, cax=cax)
-    
-    fig.tight_layout()
+    plt.colorbar(img, cax=cax)
 
-def plot_path_q(path, grid, q_hat):
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
-    
-    img1 = ax1.imshow(np.max(q_hat, axis=-1).reshape(grid.shape), cmap='hot_r')
-    ax1.set_title(r"$\max_a \ \hat{Q}(s, a)$")
-    ax1.set_xticks([])  # remove x-axis ticks
-    ax1.set_yticks([])  # remove y-axis ticks
 
-    divider = make_axes_locatable(ax1)
-    cax = divider.append_axes("right", size="5%", pad=0.15)
-    fig.colorbar(img1, cax=cax)
+def plot_maze_path_q(grid, q_star, path, axes=None):
+    if axes is None:
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+    else:
+        ax1, ax2 = axes
     
-    viz_path(path, grid, ax2)
-    ax2.set_title("Path found")
-    ax2.set_xticks([])  # remove x-axis ticks
-    ax2.set_yticks([])  # remove y-axis ticks
+    plot_maze(grid, ax1)
+    viz_path(path, grid, ax1)
+    ax1.set_title("Maze and path")
+    
+    plot_q(q_star, grid, ax2)
+    ax2.set_title(r"$\max_a Q^*(s)$")
 
-    fig.tight_layout()
 
 def plot_errors(errors, step_plot=100, ax=None):
     if ax is None:
